d4e5f0e539bb2f48cc61cf67ee7deb49080bdffa
1201wah <1201wah@gmail.com>
2020-05-22 00:56:52 +1000

    Finish root (home) page with filter & user

---
+++ app/assets/javascripts/categories.js
@@ -0,0 +1,33 @@
+document.addEventListener('turbolinks:load', ()=>{ 
+    document.querySelectorAll(".filter").forEach( el=>{
+        el.addEventListener("click", ev=>{
+            let url = new URL(document.location);
+            if (ev.target.classList.contains("active")) {
+                ev.target.classList.remove("active");
+            } else {
+                ev.target.classList.add("active");
+            }
+            url.searchParams.delete("category[]");
+            document.querySelectorAll(".filter.active").forEach( el2=> {
+                url.searchParams.append("category[]", el2.value);
+            })
+            document.location = url;
+        })
+    })
+
+    document.querySelectorAll(".to-recent").forEach( el=>{
+        el.addEventListener("click", ev=>{
+            let url = new URL(document.location);
+            url.searchParams.delete("filtered");
+            document.location = url;  
+        })
+    })
+
+    document.querySelectorAll(".to-filtered").forEach( el=>{
+        el.addEventListener("click", ev=>{
+            let url = new URL(document.location);
+            url.searchParams.append("filtered", 1);
+            document.location = url;
+        })
+    })
+}, false)
